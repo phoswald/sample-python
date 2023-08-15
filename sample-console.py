e0aafbcc6fc565cd66a8e709ae4df979417d0f43
Philip Oswald <philip.oswald@gmail.com>
2023-08-15 13:29:57 +0200

    float() und try / except

--- sample-console.py
+++ sample-console.py
@@ -5,6 +5,19 @@ def clear_console():
     #print("")
     print("\033c")
 
+def read_münze():
+    while True:
+        str = input("Münze? ")
+        if str == "exit":
+            return -1
+        try:
+            wert = float(str)
+            if wert == 0.05 or wert == 0.1 or wert == 0.2 or wert == 0.5 or wert == 1 or wert == 2 or wert == 5:
+                return wert
+            print("Keine gültige Münze! Probier nochmal.")
+        except ValueError:
+            print("Nicht mal eine Zahl, du Affe! Probier nochmal, oder 'exit' um aufzugeben.")
+
 name = input("Wer bist du? ")
 
 clear_console()
@@ -15,7 +28,9 @@ clear_console()
 time.sleep(0.5)
 
 clear_console()
-print("Ciao!")
-time.sleep(0.5)
 
-clear_console()
+wert = read_münze() # liefert einen Wert oder -1 um zu beenden
+if wert != -1:
+    print(f"OK, CHF {wert}")
+
+print("Adieu")
